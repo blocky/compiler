4d82eb3804755fa25cf6afeef6c460b53901fd49
David Millman <dave@cs.unc.edu>
2025-11-11 08:23:17 -0700

    [BKY-7387] Add bky-c to the flake (#26)
    * Add bky-c to the flake
    
    * Update version

--- flake.nix
+++ flake.nix
@@ -25,6 +25,16 @@
 
       in
       {
+        packages.bky-c = pkgs.buildGoModule {
+          pname = "bky-c";
+          version = "${self.shortRev or "dirty"}";
+
+          src = ./.;
+
+          vendorHash = "sha256-yIs80Le6rbksqZyNF1PA7DPkKokz+QYexH1UwkItncU=";
+          doCheck = false;
+        };
+
         devShells.default = pkgs.mkShell {
           name = "bky-c-dev-shell";
           packages = [
